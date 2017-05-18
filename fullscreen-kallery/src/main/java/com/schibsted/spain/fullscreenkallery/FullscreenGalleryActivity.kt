6920de5d851fcc93a82653553a264656a6cc8a50
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-05-18 14:01:49 +0200

    add resources

---
+++ fullscreen-kallery/src/main/java/com/schibsted/spain/fullscreenkallery/FullscreenGalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.schibsted.spain.fullscreenkallery
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FullscreenGalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fullscreen_gallery)
+    }
+}

84f68a1168676ed9ec2fa2081fa0cbbec18f6278
Denys Novoa <denys.novoa@scmspain.com>
2017-06-01 18:07:02 +0200

    change to arraylistOf url images sample (#1)

--- sample/src/main/java/com/schibsted/spain/sample/MainActivity.kt
+++ sample/src/main/java/com/schibsted/spain/sample/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.schibsted.spain.fullscreenkallery.FullscreenGalleryActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,41 +14,26 @@ class MainActivity : AppCompatActivity() {
 
         launcherButton.setOnClickListener {
             val galleryIntent = Intent(this@MainActivity, FullscreenGalleryActivity::class.java)
-            val imageUrlsList = ArrayList<String>()
 
-            imageUrlsList.add(
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/The_Simpsons_Logo.svg/2000px" + "-The_Simpsons_Logo.svg.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Homer/swsb_c" + "haracter_fact_homer_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Marge/swsb_c" + "haracter_fact_marge_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_bart_550x96" + "0.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Lisa/swsb_ch" + "aracter_fact_lisa_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Maggie/swsb_" + "character_fact_maggie_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_snowball_55" + "0x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_santaslittl" + "ehelper_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Krusty/swsb_" + "character_fact_krusty_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_sideshowmel" + "_550x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_krabappel_5" + "50x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_skinner_550" + "x960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_cletus_550x" + "960.png")
-            imageUrlsList.add(
-                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_brandine_55" + "0x960.png")
-            imageUrlsList.add(
-                    "http://vignette4.wikia.nocookie.net/lossimpson/images/c/c4/The_Simpsons_characters.png" + "/revision/20110629063200?path-prefix=es")
+            val imageUrlsList = arrayListOf(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/The_Simpsons_Logo.svg/2000px-The_Simpsons_Logo.svg.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Homer/swsb_character_fact_homer_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Marge/swsb_character_fact_marge_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_bart_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Lisa/swsb_character_fact_lisa_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Maggie/swsb_character_fact_maggie_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_snowball_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_santaslittlehelper_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/simpsons-character/Krusty/swsb_character_fact_krusty_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_sideshowmel_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_krabappel_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_skinner_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_cletus_550x960.png",
+                    "http://assets.fxnetworks.com/shows/the-simpsons/photos/swsb_character_fact_brandine_550x960.png",
+                    "http://vignette4.wikia.nocookie.net/lossimpson/images/c/c4/The_Simpsons_characters.png/revision/20110629063200?path-prefix=es"
+            )
 
-            galleryIntent.putStringArrayListExtra(FullscreenGalleryActivity.EXTRA_LIST_ITEMS,
-                    imageUrlsList)
+            galleryIntent.putStringArrayListExtra(FullscreenGalleryActivity.EXTRA_LIST_ITEMS, imageUrlsList)
 
             startActivity(galleryIntent)
         }

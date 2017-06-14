67dce01afb315ffc614b93aa3cd1e5badf2fe012
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-06-14 14:55:17 +0200

    Add hacky help

--- fullscreen-kallery/src/main/java/com/schibsted/spain/fullscreenkallery/recyclerviewpager/HackyRecyclerViewPager.kt
+++ fullscreen-kallery/src/main/java/com/schibsted/spain/fullscreenkallery/recyclerviewpager/HackyRecyclerViewPager.kt
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 import android.view.MotionEvent
 
-
+/**
+ * This class is because this issue: https://github.com/chrisbanes/PhotoView#issues-with-viewgroups
+ */
 class HackyRecyclerViewPager(context: Context, attrs: AttributeSet) : RecyclerView(context, attrs) {
 
   override fun onInterceptTouchEvent(e: MotionEvent?) = try {
